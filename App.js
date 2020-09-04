8be8b1d214c6e4980c3e4e9446c74bc72e52e1bb
emoticbox <emoticbox@emoticboxui-Macmini.local>
2020-09-04 20:41:15 +0900

    add : tab navigation

--- App.js
+++ App.js
@@ -11,83 +11,67 @@ import {
   StyleSheet,
   View,
   Text,
-  Button,
   TouchableOpacity
 } from 'react-native';
+// Button 사용금지
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen({ navigation }){
+function FirstScreen(){
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <TouchableOpacity
+      {/* <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate("Details")}
       >
         <View>
           <Text style={{ fontSize: 30, color: 'red' }}>Go To Hell</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   )
 }
-function DetailsScreen({ navigation }) {
+function SecondScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>This is Hell</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.push('Details')}
-      >
-        <View>
-          <Text style={{ fontSize: 30, color: 'red' }}>Go to Hell...again</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Home")}
-      >
-        <View>
-          <Text style={{ fontSize: 30, color: 'red' }}>Go to Home</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.goBack()}
-      >
-        <View>
-          <Text style={{ fontSize: 30, color: 'red' }}>Go Back</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.popToTop()}
-      >
-        <View>
-          <Text style={{ fontSize: 30, color: 'red' }}>Go back to first screen in stack</Text>
-        </View>
-      </TouchableOpacity>
+      <Text>SecondScreen</Text>
     </View>
   );
 }
-const Stack = createStackNavigator();
+function ThirdScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>ThirdScreen</Text>
+    </View>
+  );
+}
+function ForthScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>ForthScreen</Text>
+    </View>    
+  );
+}
+function FifthScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>FifthScreen</Text>
+    </View>    
+  );
+}
+const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="First" component={FirstScreen} />
+        <Tab.Screen name="Second" component={SecondScreen} />
+        <Tab.Screen name="Third" component={ThirdScreen} />
+        <Tab.Screen name="Forth" component={ForthScreen} />
+        <Tab.Screen name="Fifth" component={FifthScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
